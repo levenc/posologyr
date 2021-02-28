a71f235fba89180d2915a8b430f9363e10605d8f
Cyril Leven <cyril.leven@chu-brest.fr>
2021-02-28 18:36:53 +0100

    fix compiled models for windows,.onLoad calls RxODE on invalid models, see #3

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.onLoad <- function(libname, pkgname){
+  compiled_models <- ls('package:posologyr', pattern='mod_')
+  for (i in 1:length(compiled_models)){
+    #get and assign allow
+    if (!get(compiled_models[i])$ppk_model$isValid()){
+      assign(compiled_models[i],
+             `[[<-`(get(compiled_models[i]), 'ppk_model',
+             value = RxODE::RxODE(get(compiled_models[i]$ppk_model))),
+             envir = package:posologyr)
+    }
+  }
+  invisible()
+}
