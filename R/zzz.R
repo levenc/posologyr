1e109b75b56d53048698ff9be2724ffa853b1025
Cyril Leven <cyril.leven@chu-brest.fr>
2021-03-01 13:12:07 +0100

    fix broken install, experiment with delayedAssign, see #3

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,20 @@
-.onLoad <- function(libname, pkgname){
-  compiled_models <- ls('package:posologyr',pattern='mod_',
-                        envir = as.environment('package:posologyr'))
-  for (i in 1:length(compiled_models)){
-    #get and assign allow
-    if (!get(compiled_models[i])$ppk_model$isValid()){
-      assign(compiled_models[i],
-             `[[<-`(get(compiled_models[i]), 'ppk_model',
-             value = RxODE::RxODE(get(compiled_models[i]$ppk_model))),
-             envir = as.environment('package:posologyr'))
-    }
-  }
-  invisible()
-}
+#.onLoad <- function(libname, pkgname){
+#  compiled_models <- data(package = pkgname)
+#  compiled_models <- compiled_models$results[,3]
+#  for (i in 1:length(compiled_models)){
+#   #if(TRUE){
+#   #  print(get(compiled_models[1], envir = parent.env(environment())))
+#   #}
+#   #if (!get(compiled_models[1],
+#   #        envir = parent.env(environment()))$ppk_model$isValid()){
+#   #  print(get(compiled_models[1], envir = parent.env(environment())))
+#   #  assign(compiled_models[i],
+#   #         `[[<-`(get(compiled_models[1],
+#   #                    envir = parent.env(environment())), 'ppk_model',
+#   #         value = RxODE::RxODE(get(compiled_models[1],
+#   #                                  envir = parent.env(environment()))$ppk_model)),
+#   #         envir = parent.env(environment()))
+#   #}
+# }
+# invisible()
+#}
