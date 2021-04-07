84b440b4e519cb2c39733f00b7fab6bbe225709f
Cyril Leven <cyril.leven@chu-brest.fr>
2021-04-07 17:18:31 +0200

    apply AGPL license to all .R files

--- R/estim_functions.R
+++ R/estim_functions.R
@@ -1,4 +1,22 @@
 #-------------------------------------------------------------------------
+# posologyr: individual dose optimisation using population PK
+# Copyright (C) 2021  Cyril Leven
+#
+#    This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------
 #  Adapted from: http://shiny.webpopix.org/mcmc/bayes1/
 #  Marc Lavielle, Inria Saclay (June 28th, 2015) CeCILL-B
 #
