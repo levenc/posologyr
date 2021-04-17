3fea4b3f1d88b3236095c9289c9741b055ce5ebc
Cyril Leven <cyril.leven@chu-brest.fr>
2021-04-18 00:47:02 +0200

    add tests, see #4

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(posologyr)
+
+test_check("posologyr")
---
+++ tests/testthat/test_map.R
@@ -0,0 +1,24 @@
+library("posologyr")
+
+df_michel <- data.frame(ID=1,TIME=c(0.0,0.5,1.0,14.0),
+                        DV=c(NA,NA,25.0,5.5),
+                        AMT=c(1000,-1000,0,0),
+                        EVID=c(10102,10102,0,0),
+                        CLCREAT=80,WT=65)
+
+mod_tobra <- mod_tobramycin_2cpt_fictional
+
+solved_model_tobra <- RxODE::rxSolve(mod_tobra$ppk_model,
+                                   c(mod_tobra$psi,
+                                     diag(mod_tobra$omega)*0),
+                                   df_michel)
+
+michel_tobra_map <- poso_estim_map(solved_model = solved_model_tobra,
+                                   prior_model = mod_tobra,
+                                   dat = df_michel,
+                                   return_model = TRUE)
+
+test_that("MAP estimates match Monolix estimates", {
+  expect_equal(round(michel_tobra_map[[2]]$ke[1],3), 0.126)
+  expect_equal(round(michel_tobra_map[[2]]$V[1],1), 18.2)
+}) # 3 significant digits
