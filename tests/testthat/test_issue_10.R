9aa35a2629a76d84ac26da8b9bd242051ede41fb
Cyril Leven <cyril.leven@chu-brest.fr>
2021-05-13 16:11:07 +0200

    fix issue #10

--- tests/testthat/test_issue_10.R
+++ tests/testthat/test_issue_10.R
@@ -9,10 +9,12 @@ p01_tobra <- posologyr(prior_model=mod_tobramycin_2cpt_fictional,
 
 test_that("poso_mcmc_pop accepts burn_in == n_iter", {
   expect_equal(class(poso_estim_mcmc(p01_tobra,burn_in = 20,
-                                     n_iter = 20)[[1]]),"numeric")
+                                     n_iter = 20,
+                                     return_model = FALSE)[[1]]),"numeric")
 })
 
 test_that("poso_mcmc_pop accepts burn_in < n_iter", {
   expect_equal(class(poso_estim_mcmc(p01_tobra,burn_in = 22,
-                                     n_iter = 20)[[1]]),"numeric")
+                                     n_iter = 20,
+                                     return_model = FALSE)[[1]]),"numeric")
 })
