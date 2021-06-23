62b157e7face3c4317bd0ad42cffbae71b94f148
levenc <cyril.leven@chu-brest.fr>
2021-06-23 14:09:46 +0200

    check that the omega matrix and the vector of thetas match the RxODE model

--- R/input_validation.R
+++ R/input_validation.R
@@ -18,4 +18,15 @@ validate_priormod <- function(priormod){
                                         posologyr model")
   if (is.null(priormod$sigma)) stop("sigma is missing from the prior
                                         posologyr model")
+
+  # ETA == 0 are not needed in ppk_model$params, only check for ETA > 0
+  ind_eta      <- which(diag(priormod$omega)>0)
+  omega_eta    <- priormod$omega[ind_eta,ind_eta]
+
+  if (FALSE %in% (attr(omega_eta,"dimnames")[[1]] %in% priormod$ppk_model$params)){
+    stop("The names of the omega matrix do not match the parameters of ppk_model")
+  }
+  if (FALSE %in% (attr(priormod$theta,"names") %in% priormod$ppk_model$params)){
+    stop("The names of the theta vector do not match the parameters of ppk_model")
+  }
 }
