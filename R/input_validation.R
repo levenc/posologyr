050fe1da4967c6ea18caa9012a093005b9367896
levenc <cyril.leven@chu-brest.fr>
2021-06-25 13:28:07 +0200

    fix typos in input validation

--- R/input_validation.R
+++ R/input_validation.R
@@ -1,8 +1,8 @@
 validate_dat <- function(tdm_data){
   if (is.null(tdm_data$TIME)) stop("TIME is missing from the patient record")
   if (is.null(tdm_data$EVID)) stop("EVID is missing from the patient record")
-  if (is.null(tdm_data$AMT)) stop("TIME is missing from the patient record")
-  if (is.null(tdm_data$DV)) stop("TIME is missing from the patient record")
+  if (is.null(tdm_data$AMT)) stop("AMT is missing from the patient record")
+  if (is.null(tdm_data$DV)) stop("DV is missing from the patient record")
 }
 
 validate_priormod <- function(priormod){
@@ -12,7 +12,7 @@ validate_priormod <- function(priormod){
                                         posologyr model")
   if (is.null(priormod$theta)) stop("theta is missing from the prior
                                         posologyr model")
-  if (is.null(priormod$omega)) stop("error_model is missing from the prior
+  if (is.null(priormod$omega)) stop("omega is missing from the prior
                                         posologyr model")
   if (is.null(priormod$covariates)) stop("covariates is missing from the prior
                                         posologyr model")
