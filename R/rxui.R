6228ecc95ab1519daa40c89f4b9beb07eb3798f2
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-12-08 18:25:34 -0600

    Change model information

--- R/rxui.R
+++ R/rxui.R
@@ -3,11 +3,11 @@
 #' @param line line to parse
 #'
 #' @return error lines for posology
-#' 
+#'
 #' @export
-#' 
+#'
 #' @keywords internal
-#' 
+#'
 #' @author Matthew L. Fidler
 #'
 posologyr_error_lines <- function(line) {
@@ -16,18 +16,22 @@ posologyr_error_lines <- function(line) {
 
 #' @rdname posologyr_error_lines
 #' @export
-posologyr_error_lines.norm <- function(line) { 
+posologyr_error_lines.norm <- function(line) {
   env <- line[[1]]
   pred1 <- line[[2]]
-  .ret <- vector("list", 6)
+  ret <- vector("list", 6)
   .yj <- as.double(pred1$transform) - 1
-  .ret[[1]] <- bquote(rx_yj_ ~ .(.yj))
-  .ret[[2]] <- bquote(rx_lambda_~.(rxode2::.rxGetLambdaFromPred1AndIni(env, pred1)))
-  .ret[[3]] <- bquote(rx_low_ ~ .(rxode2::.rxGetLowBoundaryPred1AndIni(env, pred1)))
-  .ret[[4]] <- bquote(rx_hi_ ~ .(rxode2::.rxGetHiBoundaryPred1AndIni(env, pred1)))
-  .ret[[5]] <- bquote(rx_pred_f_ ~ .(rxode2::.rxGetPredictionF(env, pred1)))
-  .ret[[6]] <- bquote(rx_pred_ <- .(rxode2::.rxGetPredictionFTransform(env, pred1, .yj)))
-  .ret
+  ret[[1]] <- bquote(rx_yj_ ~ .(.yj))
+  ret[[2]] <- bquote(rx_lambda_~.(rxode2::.rxGetLambdaFromPred1AndIni(env, pred1)))
+  ret[[3]] <- bquote(rx_low_ ~ .(rxode2::.rxGetLowBoundaryPred1AndIni(env, pred1)))
+  ret[[4]] <- bquote(rx_hi_ ~ .(rxode2::.rxGetHiBoundaryPred1AndIni(env, pred1)))
+  ret[[5]] <- bquote(rx_pred_f_ ~ .(rxode2::.rxGetPredictionF(env, pred1)))
+  if (length(env$predDf$cond) == 1L) {
+    ret[[6]] <- bquote(Cc <- .(rxode2::.rxGetPredictionFTransform(env, pred1, .yj)))
+  } else {
+    ret[[6]] <- bquote(.(str2lang(paste0("rxEndpoint", pred1$dvid))) <- .(rxode2::.rxGetPredictionFTransform(env, pred1, .yj)))
+  }
+  ret
 }
 
 #' @rdname posologyr_error_lines
@@ -68,21 +72,31 @@ posologyr_error_lines.rxUi <- function(line) {
 #' @export
 rxUiGet.posologyr_ppk_model <- function(x, ...) {
   ui <- x[[1]]
+  if (is.null(ui$predDf)) {
+    stop("need endpoint defined for now")
+  }
+  if (length(ui$predDf$cond) == 1L) {
+    # Here Cc  needs to be the endpoint
+    mv <- rxode2::rxModelVars(ui)
+    if (any(mv$lhs == "Cc")) {
+      ui <- rxode2::rxRename(ui, rxCc=Cc)
+    }
+  }
   rxode2::rxCombineErrorLines(ui, errLines=posologyr_error_lines(ui),
-                              cmtLines=FALSE, dvidLine=FALSE)  
+                              cmtLines=FALSE, dvidLine=FALSE)
 }
 attr(rxUiGet.posologyr_ppk_model, "desc") <- "posologyr ppk_model element"
 
 #' @export
 rxUiGet.posologyr_error_model <- function(x, ...) {
-  
+
 }
 attr(rxUiGet.posologyr_error_model, "desc") <- "posologyr error_model element"
 
 
 #' @export
 rxUiGet.posologyr_sigma <- function(x, ...) {
-  
+
 }
 
 #' @export
