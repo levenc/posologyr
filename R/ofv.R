9fd0f22b88f02ebafb2a5dd70e4c5fab0ec7975d
levenc <cyril.leven@chu-brest.fr>
2026-03-31 11:21:44 +0200

    Scale down an overconfident error message

--- R/ofv.R
+++ R/ofv.R
@@ -33,7 +33,7 @@ objective_function <- function(y_obs=NULL,f=NULL,g=NULL,
     # if any observation used to compute the OFV is NA, the result will be NA
     stop("At least one observation (DV) is NA, the OFV can not be computed. Removing
   any row where the value in column DV is NA and the value in column EVID is 0
-  should resolve this issue.")
+  may resolve this issue.")
   }
 
   # 1) When the prediction f is zero, g can be zero (depending on the residual
